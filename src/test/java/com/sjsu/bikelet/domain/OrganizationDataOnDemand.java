abb256df27355715a208b2014760d01d800416b9
nikitham <nikitha.mamidi@gmail.com>
2013-02-04 23:05:02 -0800

    removed organization files

--- src/test/java/com/sjsu/bikelet/domain/OrganizationDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.sjsu.bikelet.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Organization.class)
-public class OrganizationDataOnDemand {
-}
