e4f585cae2bb5cb8116f9e97275706a6f18c608f
priyanka <priyanka.tarkas@gmail.com>
2013-02-19 15:49:01 -0800

    subscription rate changes

--- src/main/java/com/sjsu/bikelet/service/SubscriptionRateService.java
+++ src/main/java/com/sjsu/bikelet/service/SubscriptionRateService.java
@@ -1,7 +1,16 @@
 package com.sjsu.bikelet.service;
 
+import java.util.List;
+
 import org.springframework.roo.addon.layers.service.RooService;
+
+import com.sjsu.bikelet.domain.SubscriptionPolicy;
+import com.sjsu.bikelet.domain.SubscriptionRate;
 
 @RooService(domainTypes = { com.sjsu.bikelet.domain.SubscriptionRate.class })
 public interface SubscriptionRateService {
+	
+	public abstract List<SubscriptionRate> findAllSubscriptionRatesByPolicy(Long policyId);
+	public abstract long countSubscriptionRatesByPolicy(Long policyId);
+	public abstract List<SubscriptionRate> findSubscriptionRateEntriesByPolicy(Long policyId, int firstResult, int maxResults);
 }
--- src/main/java/com/sjsu/bikelet/service/SubscriptionRateServiceImpl.java
+++ src/main/java/com/sjsu/bikelet/service/SubscriptionRateServiceImpl.java
@@ -1,5 +1,29 @@
 package com.sjsu.bikelet.service;
 
+import java.util.List;
+
+import com.sjsu.bikelet.domain.SubscriptionPolicy;
+import com.sjsu.bikelet.domain.SubscriptionRate;
+
 
 public class SubscriptionRateServiceImpl implements SubscriptionRateService {
+
+	@Override
+	public List<SubscriptionRate> findAllSubscriptionRatesByPolicy(Long policyId) {
+		// TODO Auto-generated method stub
+		return SubscriptionRate.findAllSubscriptionRatesByPolicy(policyId);
+	}
+
+	@Override
+	public long countSubscriptionRatesByPolicy(Long policyId) {
+		// TODO Auto-generated method stub
+		return SubscriptionRate.countSubscriptionRatesByPolicy(policyId);
+	}
+
+	@Override
+	public List<SubscriptionRate> findSubscriptionRateEntriesByPolicy(
+			Long policyId, int firstResult, int maxResults) {
+		// TODO Auto-generated method stub
+		return SubscriptionRate.findSubscriptionRateEntriesByPolicy(policyId,firstResult,maxResults);
+	}
 }
