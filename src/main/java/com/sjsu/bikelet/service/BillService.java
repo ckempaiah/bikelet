97c72c80280e86b1a589c0914e99c51b6e9199a3
nikitham <nikitha.mamidi@gmail.com>
2013-03-05 13:15:59 -0800

    Update BillService.java
    methods to return bills for user and tenant

--- src/main/java/com/sjsu/bikelet/service/BillService.java
+++ src/main/java/com/sjsu/bikelet/service/BillService.java
@@ -4,4 +4,16 @@ import org.springframework.roo.addon.layers.service.RooService;
 
 @RooService(domainTypes = { com.sjsu.bikelet.domain.Bill.class })
 public interface BillService {
+  
+  public abstract List<Bill> findBillEntriesByUser(int firstResult, int sizeNo, Long userId);
+  
+	public abstract List<Bill> findAllBillsByUser(Long userId);
+	
+	public abstract long countAllBillsForUser(Long userId);
+	
+	public abstract List<Bill> findBillEntriesByTenant(int firstResult, int sizeNo, Long tenantId);
+	
+	public abstract List<Bill> findAllBillsByTenant(Long tenantId);
+	
+	public abstract long countAllBillsForTenant(Long tenantId);
 }
