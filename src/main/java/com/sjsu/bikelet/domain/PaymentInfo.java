f66e79ec9654e40912d72750e3f5d8f9744fb4a0
nikitham <nikitha.mamidi@gmail.com>
2013-03-08 23:24:22 -0800

    added cvv number, exp month, exp year in the PaymentInfo

--- src/main/java/com/sjsu/bikelet/domain/PaymentInfo.java
+++ src/main/java/com/sjsu/bikelet/domain/PaymentInfo.java
@@ -2,6 +2,10 @@ package com.sjsu.bikelet.domain;
 
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
 import javax.validation.constraints.Size;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
@@ -23,7 +27,45 @@ public class PaymentInfo {
     @ManyToOne
     private BikeLetUser userId;
     
-    public static PaymentInfo findPaymentInfoByUser(Long userId)
+    @NotNull
+    @Min(100)
+    @Max(9999)
+    private Integer cvv;
+    
+    @NotNull
+    @Min(1)
+    @Max(12)
+    private Integer cardExpMonth;
+    
+    @NotNull
+    @Min(2013)
+    private Integer cardExpYear;
+    
+    public Integer getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(Integer cvv) {
+		this.cvv = cvv;
+	}
+
+	public Integer getCardExpMonth() {
+		return cardExpMonth;
+	}
+
+	public void setCardExpMonth(Integer cardExpMonth) {
+		this.cardExpMonth = cardExpMonth;
+	}
+
+	public Integer getCardExpYear() {
+		return cardExpYear;
+	}
+
+	public void setCardExpYear(Integer cardExpYear) {
+		this.cardExpYear = cardExpYear;
+	}
+
+	public static PaymentInfo findPaymentInfoByUser(Long userId)
     {
     	return entityManager().createQuery("SELECT o FROM PaymentInfo o where o.userId.id = :userId", PaymentInfo.class)
     				   .setParameter("userId",userId)
